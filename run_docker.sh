d1079507c70933b18dd7d3cdd352740b8cfe8965
ycx <67336525@qq.com>
2020-11-12 17:41:29 +0800

    README

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 docker run --rm -it --privileged \
            -e VPN_SERVER_IPV4='{proxy ip}' \
-           -e VPN_PSK='vpn' \
-           -e VPN_USERNAME='vpn' \
-           -e VPN_PASSWORD='vpn' \
-           -e SS_PASSWORD='*mm5wd1a' \
-              40627a1bb82c
+           -e VPN_PSK='{psk}' \
+           -e VPN_USERNAME='{username}' \
+           -e VPN_PASSWORD='{password}' \
+           -e SS_PASSWORD='{ss_password}' \
+              {container_id}

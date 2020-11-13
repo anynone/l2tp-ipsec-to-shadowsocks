ac7df36696caffd4051b97c81b791577eb6c657d
ycx <67336525@qq.com>
2020-11-13 09:40:51 +0800

    删除启动脚本

--- run_docker.sh
+++
@@ -1,7 +0,0 @@
-docker run --rm -it --privileged \
-           -e VPN_SERVER_IPV4='{proxy ip}' \
-           -e VPN_PSK='{psk}' \
-           -e VPN_USERNAME='{username}' \
-           -e VPN_PASSWORD='{password}' \
-           -e SS_PASSWORD='{ss_password}' \
-              {container_id}
